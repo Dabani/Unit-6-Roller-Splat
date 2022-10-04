020e236067d32ebc21ce5780e0eae42d2be3c9eb
Dabani <robertmaku@yahoo.fr>
2022-10-04 12:53:48 +0200

    Write code to control the color changes

--- Assets/Scripts/GroundPiece.cs
+++ Assets/Scripts/GroundPiece.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class GroundPiece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isColored = false;
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeColor(Color color)
     {
-        
+        GetComponent<MeshRenderer>().material.color = color;
+        isColored = true;
     }
 }
