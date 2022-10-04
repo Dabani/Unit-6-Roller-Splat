8f7bcabad48818013d0bc4685170abfe71460bd0
Dabani <robertmaku@yahoo.fr>
2022-10-04 19:52:08 +0200

    Add rigid body to the ball

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 15;
+}
