fa134befee1ae25bf0c86f5abbab5990a907ca9b
Dabani <robertmaku@yahoo.fr>
2022-10-05 17:48:08 +0200

    Ball Controller, manage moves

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,7 +7,84 @@ public class BallController : MonoBehaviour
     public Rigidbody rb;
     public float speed = 15;
 
+    private bool isTraveling;
+    private Vector3 travelDirection;
+    private Vector3 nextCollisionPosition;
+
+    public int minSwipeRecognition = 500;
+    private Vector2 swipePosLastFrame;
+    private Vector2 swipePosCurrentFrame;
+  private Vector2 currentSwipe;
+
     private void FixedUpdate() {
-        rb.velocity = speed * Vector3.forward;
+        if (isTraveling)
+        {
+            rb.velocity = speed * travelDirection;
+        }
+        
+        if (nextCollisionPosition != Vector3.zero)
+        {
+            if (Vector3.Distance(transform.position, nextCollisionPosition) < 1)
+            {
+                isTraveling = false;
+                travelDirection = Vector3.zero;
+                nextCollisionPosition = Vector3.zero;
+            }
+        }
+
+        if (isTraveling)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            swipePosCurrentFrame = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            if (swipePosLastFrame != Vector2.zero)
+            {
+                currentSwipe = swipePosCurrentFrame - swipePosLastFrame;
+
+                if (currentSwipe.sqrMagnitude < minSwipeRecognition)
+                {
+                    return;
+                }
+
+                currentSwipe.Normalize();
+
+                // Up/Down
+                if (currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+                    // Go Up/Down
+                    SetDestination(currentSwipe.y > 0 ? Vector3.forward : Vector3.back);
+                }
+                
+                if (currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    // Go Left/Right
+                    SetDestination(currentSwipe.x > 0 ? Vector3.right : Vector3.left);
+                }
+            }
+
+            swipePosLastFrame = swipePosCurrentFrame;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            swipePosLastFrame = Vector2.zero;
+            currentSwipe = Vector2.zero;
+        }
+    }
+    private void SetDestination(Vector3 direction)
+    {
+        travelDirection = direction;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction, out hit, 100f))
+        {
+            nextCollisionPosition = hit.point;
+        }
+
+        isTraveling = true;
     }
 }
