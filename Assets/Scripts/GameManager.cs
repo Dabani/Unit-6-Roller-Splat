e8330ab10bfada13ebe65b7ab398750a6936abbe
Dabani <robertmaku@yahoo.fr>
2022-10-05 20:14:08 +0200

    Game manager control scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,5 +15,52 @@ public class GameManager : MonoBehaviour
     private void SetupNewLevel()
     {
         allGroundPieces = FindObjectsOfType<GroundPiece>();
+    }
+
+    //Manage levels & scenes
+    private void Awake() 
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+        }
+        else if (singleton != this)
+        {
+            Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void OnEnable() {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        SetupNewLevel();
+    }
+
+    public void CheckComplete()
+    {
+        bool isFinished = true;
+
+        for (int i = 0; i < allGroundPieces.Length; i++)
+        {
+            if (allGroundPieces[i].isColored == false)
+            {
+                isFinished = false;
+                break;
+            }
+        }
+
+        if (isFinished)
+        {
+            NextLevel();
+        }
+    }
+
+    private void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 1);
     }
 }
